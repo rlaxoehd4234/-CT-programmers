4bf960f454911c9883b4e5f480adf388559ff9a1
kimtaedong <rlaxoehd4234@naver.com>
2023-03-14 00:28:30 +0900

    //code

--- programmers/src/basic_algo/BFS.java
+++ programmers/src/basic_algo/BFS.java
@@ -11,16 +11,16 @@ public class BFS {
     static void bfs(int node){
         boolean[] visited = new boolean[MAX_N]; // 방문한 노드를 체크해야 함.
 
-        Queue<Integer> queue = new LinkedList<>();
-        visited[node] = true;
-        queue.add(node);
+        Queue<Integer> queue = new LinkedList<>(); // 노드를 담아주는 큐 (선입선출 컬렉션)
+        visited[node] = true; // 시작점은 방문 후에 시작하기에 true 로 변경해준다.
+        queue.add(node); // 큐에 시작점을 추가한다.
 
         while(!queue.isEmpty()){
             int curr = queue.remove(); // 방문한 노드를 큐에서 제거하고
             System.out.println(curr + " "); // 최근 방문한 노드를 출력 함.
 
             for(int next = 0; next < N; ++next){
-                if(!visited[next] && Graph[curr][next] != 0){ // false 가 아니고 간선이 아니라면 비지트 하고 큐에 추가 해준다.
+                if(!visited[next] && Graph[curr][next] != 0){
                     visited[next] = true;
                     queue.add(next); // queue 에 추가해주고 나중에 제거한다.
                 }
